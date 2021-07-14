bab7b8f69fab52cf48a242ad31d01c3c06b5ba3f
Moshiko Amsalem <moshiko14691@gmail.com>
2021-07-14 13:34:41 +0300

    merged conflicts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,10 @@
 <template>
-<<<<<<< HEAD
 	<div class="home"></div>
-=======
-	<div class="home">
-	</div>
->>>>>>> 6c88a295c90b682b0b735b831dff7817f87831f3
 </template>
 
 <script>
-
 export default {
-  name: "Home",
-  components: {},
+	name: 'Home',
+	components: {},
 };
-
 </script>
