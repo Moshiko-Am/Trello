04156a1efa37ef7a65407ef3db6a4c48e39ffcb8
Moshiko Amsalem <moshiko14691@gmail.com>
2021-07-14 18:28:24 +0300

    minor changes

--- src/cmps/group.details.vue
+++ src/cmps/group.details.vue
@@ -1,41 +1,43 @@
 <template>
-  <section v-if="group" class="group-wrapper">
-    <div class="group-details">
-      <div class="group-header">
-        <div class="group-header-name-assist js-group-name-assist"></div>
-        <div class="group-header-target js-editing-target"></div>
-        <textarea
-          class="group-header-name mod-group-name js-group-name-input"
-          v-bind:aria-label="group.title"
-          spellcheck="false"
-          dir="auto"
-          maxlength="512"
-          style="overflow: hidden; overflow-wrap: break-word; height: 28px;"
-          v-model="groupTitle"
-        ></textarea>
-        <div class="group-header-extras">...</div>
-      </div>
-      <div v-for="card in group.cards" v-bind:key="card.id">
-        <card-list :card="card" />
-      </div>
-      <div class="card-composer"></div>
-    </div>
-  </section>
-  <loader v-else />
+	<section v-if="group" class="group-wrapper">
+		<div class="group-details">
+			<div class="group-header">
+				<div
+					class="group-header-name-assist js-group-name-assist"
+				></div>
+				<div class="group-header-target js-editing-target"></div>
+				<textarea
+					class="group-header-name mod-group-name js-group-name-input"
+					v-bind:aria-label="group.title"
+					spellcheck="false"
+					dir="auto"
+					maxlength="512"
+					style="overflow: hidden; overflow-wrap: break-word; height: 28px;"
+					v-model="groupTitle"
+				></textarea>
+				<div class="group-header-extras">...</div>
+			</div>
+			<div v-for="card in group.cards" v-bind:key="card.id">
+				<!-- <card-list :card="card" /> -->
+			</div>
+			<div class="card-composer"></div>
+		</div>
+	</section>
+	<loader v-else />
 </template>
 
 <script>
 export default {
-  props: {
-    group: Object,
-  },
-  data() {
-    return {
-      groupTitle: "",
-    };
-  },
-  created() {
-    this.groupTitle = this.group.title;
-  },
+	props: {
+		group: Object,
+	},
+	data() {
+		return {
+			groupTitle: '',
+		};
+	},
+	created() {
+		this.groupTitle = this.group.title;
+	},
 };
 </script>
