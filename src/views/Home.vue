9e8e7769d37070e450d439cd7246b6dc5dd27b8c
Omri Baram <omribaram@gmail.com>
2021-07-14 12:20:42 +0300

    icon font & sass

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,12 @@
 <template>
-	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png" />
-	</div>
+  <div class="icon-sm icon-member"></div>
 </template>
 
 <script>
 // @ is an alias to /src
 
 export default {
-	name: 'Home',
-	components: {},
+  name: "Home",
+  components: {},
 };
 </script>
