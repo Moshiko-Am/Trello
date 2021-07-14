78be49188c5cdbc9a6b13872a07cf708f8eb9885
Omri Baram <omribaram@gmail.com>
2021-07-14 22:42:47 +0300

    group.list.details

--- src/cmps/group.details.vue
+++ src/cmps/group.details.vue
@@ -1,71 +1,65 @@
 <template>
-	<section v-if="group">
-		<div class="group-details">
-			<div class="group-header">
-				<div class="group-header-target js-editing-target"></div>
-				<textarea
-					class="group-header-name mod-group-name"
-					v-bind:aria-label="group.title"
-					spellcheck="false"
-					dir="auto"
-					maxlength="512"
-					v-model="groupTitle"
-				></textarea>
-				<div class="group-header-extras">
-					<a
-						class="group-header-extras-menu dark-hover icon-sm icon-dots-menu"
-						href="#"
-						aria-label="List actions"
-						><div></div
-					></a>
-				</div>
-			</div>
-			<card-preview
-				v-for="card in group.cards"
-				:key="card.id"
-				:card="card"
-				@click.native="setCard(card)"
-			/>
-			<div class="card-composer-container js-card-composer-container">
-				<a class="open-card-composer js-open-card-composer" href="#">
-					<span class="icon-sm icon-add"></span>
-					<span class="js-add-a-card">Add a card</span></a
-				>
-			</div>
-		</div>
-		<card-details
-			v-if="currCard"
-			:card="currCard"
-			:group="group"
-		></card-details>
-	</section>
-	<!-- <loader v-else /> -->
+  <section v-if="group">
+    <div class="group-details">
+      <div class="group-header">
+        <textarea
+          class="group-header-name mod-group-name"
+          v-bind:aria-label="group.title"
+          spellcheck="false"
+          dir="auto"
+          maxlength="512"
+          v-model="groupTitle"
+        ></textarea>
+        <div class="group-header-extras">
+          <a class="group-header-extras-menu icon-sm icon-dots-menu"></a>
+        </div>
+      </div>
+      <card-preview
+        v-for="card in group.cards"
+        :key="card.id"
+        :card="card"
+        @click.native="setCard(card)"
+      />
+      <div class="card-composer-container js-card-composer-container">
+        <a class="open-card-composer js-open-card-composer" href="#">
+          <span class="icon-sm icon-add"></span>
+          <span class="btn-add-card">Add a card</span></a
+        >
+      </div>
+    </div>
+    <card-details
+      v-if="currCard"
+      :card="currCard"
+      :group="group"
+    ></card-details>
+  </section>
+  <!-- <loader v-else /> -->
 </template>
 
 <script>
-import cardPreview from './card.preview.vue';
-import cardDetails from './card.details.vue';
+import cardPreview from "./card.preview.vue";
+import cardDetails from "./card.details.vue";
 export default {
-	props: {
-		group: Object,
-	},
-	data() {
-		return {
-			groupTitle: '',
-			currCard: null,
-		};
-	},
-	methods: {
-		setCard(card) {
-			this.currCard = card;
-		},
-	},
-	created() {
-		this.groupTitle = this.group.title;
-	},
-	components: {
-		cardPreview,
-		cardDetails,
-	},
+  props: {
+    group: Object,
+  },
+  data() {
+    return {
+      groupTitle: "",
+      currCard: null,
+    };
+  },
+  methods: {
+    setCard(card) {
+      this.currCard = card;
+    },
+  },
+  created() {
+    this.groupTitle = this.group.title;
+  },
+  components: {
+    cardPreview,
+    cardDetails,
+  },
 };
 </script>
--- src/cmps/group.list.vue
+++ src/cmps/group.list.vue
@@ -1,26 +1,22 @@
 <template>
-	<section v-if="groups" class="group-list">
-		<div
-			v-for="group in groups"
-			v-bind:key="group.id"
-			class="group-wrapper"
-		>
-			<div class="group">
-				<group-details :group="group" />
-			</div>
-		</div>
-	</section>
-	<!-- <loader v-else /> -->
+  <section v-if="groups" class="group-list">
+    <div v-for="group in groups" v-bind:key="group.id" class="group-wrapper">
+      <div class="group">
+        <group-details :group="group" />
+      </div>
+    </div>
+  </section>
+  <!-- <loader v-else /> -->
 </template>
 
 <script>
-import groupDetails from '@/cmps/group.details.vue';
+import groupDetails from "@/cmps/group.details.vue";
 export default {
-	props: {
-		groups: Array,
-	},
-	components: {
-		groupDetails,
-	},
+  props: {
+    groups: Array,
+  },
+  components: {
+    groupDetails,
+  },
 };
 </script>
