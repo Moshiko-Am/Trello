3c5b8ab4b822536411c76833f3bd8fe8132978b1
Moshiko Amsalem <moshiko14691@gmail.com>
2021-07-14 13:27:47 +0300

    added app header with css, no logics

---
+++ src/cmps/board.header.vue
@@ -0,0 +1,13 @@
+<template>
+	<section class="board-header-container">
+		<h1>{{ board.title }}</h1>
+	</section>
+</template>
+
+<script>
+export default {
+	props: {
+		board: Object,
+	},
+};
+</script>
--- src/views/board.details.vue
+++ src/views/board.details.vue
@@ -2,11 +2,13 @@
 	<section class="board-details">
 		<p>{{ board }}</p>
 		<!-- <card-details></card-details> -->
+		<board-header :board="board" />
 	</section>
 </template>
 
 <script>
 // import cardDetails from '../cmps/card.details.vue'
+import boardHeader from '../cmps/board.header.vue';
 
 export default {
 	computed: {
@@ -16,6 +18,7 @@ export default {
 	},
 	components: {
 		// cardDetails
+		boardHeader,
 	},
 	watch: {
 		'this.$route.params.boardId': {
@@ -24,7 +27,7 @@ export default {
 				if (this.$route.params.boardId) {
 					const { boardId } = this.$route.params;
 					try {
-						await this.$store.dispatch({type:'loadBoards'})
+						await this.$store.dispatch({ type: 'loadBoards' });
 						this.$store.commit('getBoardById', boardId);
 					} catch (err) {
 						console.log('didnt find board', err);
