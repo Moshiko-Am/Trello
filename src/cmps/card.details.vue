6c88a295c90b682b0b735b831dff7817f87831f3
Tom Bechar <tomchick30@gmail.com>
2021-07-14 13:27:49 +0300

    worked on card details cmp

--- src/cmps/card.details.vue
+++ src/cmps/card.details.vue
@@ -1,63 +1,66 @@
 <template>
   <section class="card-details-bg">
     <section class="card-details">
+      <button class="close-btn"><span class="icon-md icon-x"></span></button>
       <div class="card-details-header">
         <div class="inner-container">
-        <span class="m-icon details-header-icon"></span>
-        <h2 class="card-details-title">{{card.title}}</h2>
+          <span class="icon-lg icon-card details-header-icon"></span>
+          <h2 class="card-details-title">{{ card.title }}</h2>
         </div>
-        <div class="card-details-list">From list "listname"</div>
+        <div class="card-details-list">From list {listname}</div>
       </div>
       <div class="card-details-content">
         <div class="card-details-main">
           <div class="card-main-item">
             <div class="main-item-header">
-              <img class="m-icon description-header-icon" src="@/assets/img/icons/description.png" />
+              <span class="icon-lg icon-desc"></span>
               <h3 class="main-item-title">Description</h3>
             </div>
             <div class="description-textarea">
-              <p>{{card.description}}</p>
+              <p>{{ card.description }}</p>
             </div>
           </div>
-          <div class="card-main-item">
+          <div class="checklists" v-if="card.checklists.length" >
+          <div class="card-main-item" v-for="checklist in card.checklists" :key="checklist.id">
             <div class="main-item-header">
-              <img class="m-icon description-header-icon" src="@/assets/img/icons/checked-box.png" />
-              <h3 class="main-item-title">Checklist</h3>
+              <span class="icon-lg icon-checklist"></span>
+              <h3 class="main-item-title">{{checklist.title}}</h3>
             </div>
+          </div>
           </div>
           <div class="card-main-item">
             <div class="main-item-header activity">
               <div>
-                <img class="m-icon description-header-icon" src="@/assets/img/icons/activity.png" />
+                <span class="icon-lg icon-activity"></span>
                 <h3 class="main-item-title">Activity</h3>
               </div>
-              <button>Show Details</button>
+              <button class="card-sidebar-btn">Show Details</button>
             </div>
           </div>
         </div>
         <div class="card-details-sidebar">
           <div class="card-sidebar-btn">
-            <img src="@/assets/img/icons/user.png" class="s-icon" alt="">
+            <span class="icon-sm icon-member"></span>
             <p class="sidebar-btn-title">Members</p>
           </div>
           <div class="card-sidebar-btn">
-            <img src="@/assets/img/icons/label.png" class="s-icon" alt="">
+            <span class="icon-sm icon-label"></span>
             <p class="sidebar-btn-title">Labels</p>
           </div>
           <div class="card-sidebar-btn">
-            <img src="@/assets/img/icons/checked-box.png" class="s-icon" alt="">
+            <span class="icon-sm icon-checklist"></span>
             <p class="sidebar-btn-title">Checklist</p>
           </div>
           <div class="card-sidebar-btn">
-            <img src="@/assets/img/icons/clock.png" class="s-icon" alt="">
+            <span class="icon-sm icon-date"></span>
             <p class="sidebar-btn-title">Dates</p>
           </div>
           <div class="card-sidebar-btn">
-            <img src="@/assets/img/icons/attachments.png" class="s-icon" alt="">
+            <span class="icon-sm icon-attach"></span>
             <p class="sidebar-btn-title">Attachments</p>
           </div>
           <div class="card-sidebar-btn">
-            <img src="@/assets/img/icons/cover.png" class="s-icon" alt="">
+            <span class="icon-sm icon-cover"></span>
             <p class="sidebar-btn-title">Cover</p>
           </div>
         </div>
@@ -73,7 +76,8 @@ export default {
       card: {
         id: "c104",
         title: "Help me",
-        description: "I need to make a very special super duper app that looks like trello",
+        description:
+          "I need to make a very special super duper app that looks like trello",
         comments: [
           {
             id: "ZdPnm",
@@ -90,7 +94,7 @@ export default {
         checklists: [
           {
             id: "YEhmF",
-            title: "Checklist",
+            title: "Checklist 1",
             todos: [
               {
                 id: "212jX",
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,10 @@
 </template>
 
 <script>
-// @ is an alias to /src
 
 export default {
   name: "Home",
   components: {},
 };
+
 </script>
