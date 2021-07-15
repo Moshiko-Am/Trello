2494a16b790dcce37081ca04d3aaf12ce93482e6
Tom Bechar <tomchick30@gmail.com>
2021-07-15 19:47:49 +0300

    add checklist

---
+++ src/cmps/checklist.add.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="checklist-add">
+      <span class="sm-icon icon-x" @click="close"></span>
+    <h3>Add checklist</h3>
+    <hr />
+    <label>
+      Title
+      <input type="text" placeholder="checkList" v-model="checklist.title" />
+    </label>
+    <button class="add-cl-btn" @click="addCl">Add</button>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      checkList: {
+        id: makeId(),
+        title: "",
+        todos: [],
+      },
+    };
+  },
+  methods: {
+    addCl() {
+      this.$emit("addCl", { ...this.checkList });
+      this.checkList = {
+        id: makeId(),
+        title: "",
+        todos: [],
+      };
+    },
+    close(){
+        
+    }
+  },
+};
+</script>
