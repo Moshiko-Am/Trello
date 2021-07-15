3e0a1b502f4fb16ffe1d17cd58523c4c7adc0434
Moshiko Amsalem <moshiko14691@gmail.com>
2021-07-15 18:46:06 +0300

    change header and remove members

--- src/views/board.details.vue
+++ src/views/board.details.vue
@@ -1,71 +1,75 @@
 <template>
-  <section class="board-details">
-    <board-header
-      :board="boardToEdit"
-      @boardUpdate="boardUpdate"
-      v-if="boardToEdit"
-    />
-    <group-list
-      @boardUpdate="boardUpdate"
-      v-if="boardToEdit"
-      :groups="boardToEdit.groups"
-      :labels="labels"
-    />
-  </section>
+	<section class="board-details">
+		<board-header
+			:board="boardToEdit"
+			@boardUpdate="boardUpdate"
+			v-if="boardToEdit"
+		/>
+		<group-list
+			@boardUpdate="boardUpdate"
+			v-if="boardToEdit"
+			:groups="boardToEdit.groups"
+			:labels="labels"
+		/>
+	</section>
 </template>
 
 <script>
-import boardHeader from "@/cmps/board.header.vue";
-import groupList from "@/cmps/group.list.vue";
+import boardHeader from '@/cmps/board.header.vue';
+import groupList from '@/cmps/group.list.vue';
 
 export default {
-  data() {
-    return {
-      boardToEdit: null,
-    };
-  },
-  computed: {
-    board() {
-      return this.$store.getters.board;
-    },
-    labels() {
-      const board = this.$store.getters.board;
-      return board.labels;
-    },
-  },
-  components: {
-    boardHeader,
-    groupList,
-  },
-  methods: {
-    async boardUpdate(update) {
-      this.boardToEdit[update.type] = update.payload;
-      const board = JSON.parse(JSON.stringify(this.boardToEdit));
-      try {
-        await this.$store.dispatch({ type: "saveBoard", board });
-      } catch (err) {
-        console.log(`coldn't save board`);
-      }
-    },
-  },
-  created() {},
-  watch: {
-    "this.$route.params.boardId": {
-      immediate: true,
-      async handler() {
-        if (this.$route.params.boardId) {
-          const { boardId } = this.$route.params;
-          try {
-            await this.$store.dispatch({ type: "loadBoards" });
-            this.$store.commit("getBoardById", boardId);
-            this.boardToEdit = this.$store.state.boardStore.selectedBoard;
-          } catch (err) {
-            console.log("didnt find board", err);
-            // this.$router.push(`/`);
-          }
-        }
-      },
-    },
-  },
+	data() {
+		return {
+			boardToEdit: null,
+		};
+	},
+	computed: {
+		board() {
+			return this.$store.getters.board;
+		},
+		labels() {
+			const board = this.$store.getters.board;
+			return board.labels;
+		},
+	},
+	components: {
+		boardHeader,
+		groupList,
+	},
+	methods: {
+		async boardUpdate(update) {
+			this.boardToEdit[update.type] = update.payload;
+			const board = JSON.parse(JSON.stringify(this.boardToEdit));
+			try {
+				await this.$store.dispatch({ type: 'saveBoard', board });
+			} catch (err) {
+				console.log(`coldn't save board`);
+			}
+		},
+	},
+	created() {},
+	watch: {
+		'this.$route.params.boardId': {
+			immediate: true,
+			async handler() {
+				if (this.$route.params.boardId) {
+					const { boardId } = this.$route.params;
+					try {
+						await this.$store.dispatch({ type: 'loadBoards' });
+						this.$store.commit('getBoardById', boardId);
+						this.boardToEdit = JSON.parse(
+							JSON.stringify(
+								this.$store.state.boardStore.selectedBoard
+							)
+						);
+					} catch (err) {
+						console.log('didnt find board', err);
+						// this.$router.push(`/`);
+					}
+				}
+			},
+		},
+	},
 };
 </script>
