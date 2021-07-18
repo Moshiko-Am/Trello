4062fc6fd086e0afabbf19318fca4e9f8a32dfd0
Moshiko Amsalem <moshiko14691@gmail.com>
2021-07-18 22:13:59 +0300

    home page and some othe css

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,45 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import board from '@/views/board.details.vue';
+import home from '@/views/home.page.vue';
 import boards from '@/cmps/board.list.vue';
-import loginPage from '@/views/login.page.vue'
-import canvasCmp from '@/cmps/card-details-cmps/canvas.cmp'
-import VueKonva from "vue-konva";
+import loginPage from '@/views/login.page.vue';
+import canvasCmp from '@/cmps/card-details-cmps/canvas.cmp';
+import VueKonva from 'vue-konva';
 
 Vue.use(VueRouter);
 Vue.use(VueKonva);
 
-const routes = [{
-        path: '/',
-        name: 'Home',
-        component: board,
-    },
-    {
-        path: '/boards',
-        name: 'Boards',
-        component: boards,
-    },
-    {
-        path: '/canvas',
-        name: 'Canvas',
-        component: canvasCmp,
-    },
-    {
-        path: '/board/:boardId',
-        name: 'Board',
-        component: board,
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: loginPage,
-    },
+const routes = [
+	{
+		path: '/',
+		name: 'Home',
+		component: home,
+	},
+	{
+		path: '/boards',
+		name: 'Boards',
+		component: boards,
+	},
+	{
+		path: '/canvas',
+		name: 'Canvas',
+		component: canvasCmp,
+	},
+	{
+		path: '/board/:boardId',
+		name: 'Board',
+		component: board,
+	},
+	{
+		path: '/login',
+		name: 'Login',
+		component: loginPage,
+	},
 ];
 
 const router = new VueRouter({
-    routes,
+	routes,
 });
 
 export default router;
